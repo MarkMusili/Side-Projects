977a7a0ed1ef6117525280d88588b1a239f5d0a3
v1peridae <louisakavindu@gmail.com>
2023-11-24 01:03:32 -0800

    Add link shortening functionality

---
+++ Beginner level/Tools/Link-Shortener/script.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("lsenterbutton");
+btn.addEventListener("click", short);
+
+function short() {
+    let longurl = document.getElementById("lsinputid").value;
+    console.log(longurl);
+}
